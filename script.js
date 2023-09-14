9a672f5ebe68ada7f5f95dfd33ab875c9ce6a9ef
anneantony15 <106422182+anneantony15@users.noreply.github.com>
2023-09-14 22:04:44 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function click(val)
+function buttonclick(val)
 {
-document.getElementById("screen").value+=val
+
+document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function buttonclear()
+{
+    document.getElementById("screen").value='';
+}
+function equalto()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
 }
